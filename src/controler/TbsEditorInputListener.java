e5e84666e188c5991b15cdbffe776050243e1b8b
NicolasPetetin <petetinnicolas3@gmail.com>
2021-06-28 13:51:45 +0200

    Màj automatique de l'output en fonction de l'input

--- src/controler/TbsEditorInputListener.java
+++ src/controler/TbsEditorInputListener.java
@@ -1,6 +1,7 @@
 package controler;
 
 import java.awt.Component;
+import java.util.List;
 
 import javax.swing.event.CaretEvent;
 import javax.swing.event.CaretListener;
@@ -8,6 +9,7 @@ import javax.swing.event.CaretListener;
 import guiElements.TbsEditorInput;
 import guiElements.TbsEditorOutput;
 import guiElements.TbsFrame;
+import helpers.ComponentHelper;
 
 public class TbsEditorInputListener implements CaretListener {
 	private TbsFrame tbsFrame;
@@ -19,7 +21,7 @@ public class TbsEditorInputListener implements CaretListener {
 	@Override
 	public void caretUpdate(CaretEvent arg0) {
 		TbsEditorInput editorIn = (TbsEditorInput) arg0.getSource();
-		Component[] components = tbsFrame.getRootPane().getComponents();
+		List<Component> components = ComponentHelper.getAllComponents(tbsFrame);
 		for(Component component : components) {
 			if(component instanceof TbsEditorOutput) {
 				((TbsEditorOutput) component).setText(editorIn.getText());
