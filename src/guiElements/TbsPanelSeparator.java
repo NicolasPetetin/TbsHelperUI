26cfd8ffae2d394fbf0a152469b6318592228b95
NicolasPetetin <petet@10.156.4.138>
2021-07-07 13:06:26 +0200

    Ajout bordure

---
+++ src/guiElements/TbsPanelSeparator.java
@@ -0,0 +1,12 @@
+package guiElements;
+
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
+
+public class TbsPanelSeparator extends JSeparator{
+	private static final long serialVersionUID = -7630085833142809379L;
+	
+	public TbsPanelSeparator() {
+		super(SwingConstants.HORIZONTAL);
+	}
+}
--- src/guiElements/TbsTopPanel.java
+++ src/guiElements/TbsTopPanel.java
@@ -1,5 +1,7 @@
 package guiElements;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JPanel;
 
 public class TbsTopPanel extends JPanel {
@@ -7,11 +9,19 @@ public class TbsTopPanel extends JPanel {
 	@SuppressWarnings("unused")
 	private TbsFrame tbsFrame;
 	private TbsActionComboBox actionComboBox;
+	private TbsPanelSeparator separator;
+	private TbsTopPanelBorder border;
 	
 	public TbsTopPanel(TbsFrame tbsFrame) {
 		this.tbsFrame = tbsFrame;
-				
+		
+		border = new TbsTopPanelBorder();
+		setBorder(border);
+		
 		actionComboBox = new TbsActionComboBox();
-		add(actionComboBox);
+		add(actionComboBox, BorderLayout.NORTH);
+		
+		separator = new TbsPanelSeparator();
+		add(separator, BorderLayout.SOUTH);
 	}
 }
