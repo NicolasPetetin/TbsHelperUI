9e90033994ed87d2068973a438b940acbaa027b8
NicolasPetetin <petetinnicolas3@gmail.com>
2021-07-15 12:06:22 +0200

    Items du menu bar Edition remis dans l'ordre

--- src/guiElements/TbsEditionMenu.java
+++ src/guiElements/TbsEditionMenu.java
@@ -4,8 +4,20 @@ import javax.swing.JMenu;
 
 public class TbsEditionMenu extends JMenu {
 	private static final long serialVersionUID = 7247017533706354105L;
-
+	private TbsCutMenuItem cutItem;
+	private TbsCopyMenuItem copyItem;
+	private TbsPasteMenuItem pasteItem;
+	
 	public TbsEditionMenu() {
+		this.pasteItem = new TbsPasteMenuItem();
+		add(pasteItem);
+		
+		this.cutItem = new TbsCutMenuItem();
+		add(cutItem);
+		
+		this.copyItem = new TbsCopyMenuItem();
+		add(copyItem);
+
 		setText("Edition");
 	}
 }
